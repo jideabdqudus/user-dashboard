ed9981b1ea069ed39e026c8bbdf7ef37fe636353
jideabdqudus <jideabdqudus@gmail.com>
2020-05-14 21:45:26 +0100

    Added Commit

--- front/src/views/SignIn/SignIn.js
+++ front/src/views/SignIn/SignIn.js
@@ -44,17 +44,6 @@ const useStyles = makeStyles(theme => ({
       display: 'none'
     }
   },
-  quote: {
-    backgroundColor: theme.palette.neutral,
-    height: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundImage: 'url(/images/auth.jpg)',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center'
-  },
   quoteInner: {
     textAlign: 'center',
     flexBasis: '600px'
@@ -191,13 +180,7 @@ const SignIn = props => {
         >
           <div className={classes.quote}>
             <div className={classes.quoteInner}>
-              <Typography
-                className={classes.quoteText}
-                variant="h1"
-              >
-                Hella narwhal Cosby sweater McSweeney's, salvia kitsch before
-                they sold out High Life.
-              </Typography>
+              
               <div className={classes.person}>
                 <Typography
                   className={classes.name}
@@ -222,11 +205,7 @@ const SignIn = props => {
           xs={12}
         >
           <div className={classes.content}>
-            <div className={classes.contentHeader}>
-              <IconButton onClick={handleBack}>
-                <ArrowBackIcon />
-              </IconButton>
-            </div>
+            
             <div className={classes.contentBody}>
               <form
                 className={classes.form}
